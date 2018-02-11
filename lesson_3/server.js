a3763e6f713fbcb2a04d040018c109a0789c47b2
panda <SmelayaPanda@mail.ru>
2018-02-11 15:22:19 +0700

    lesson 3 - render of HTML file

---
+++ lesson_3/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-type': 'text/html'});
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    // Here may be an error because read of file in callback function
+    // must be executed longer then outer response.end()
+    // response.end();
+}
+
+http.createServer(onRequest).listen(8000);
