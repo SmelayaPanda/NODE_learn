e49d05521b7f8a04bce9fadee9c3748de4a1f512
panda <SmelayaPanda@mail.ru>
2018-02-12 00:21:34 +0700

    lesson 6 - simple submit with routing

--- lesson_6/routes/index.js
+++ lesson_6/routes/index.js
@@ -3,13 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index',
-        {
-            title: 'Cool, huh!',
-            condition: false,
-            anyArray: [1, 2, 3]
-        }
-    );
+    res.render('index', {
+        title: 'Cool, huh!',
+        condition: false,
+        anyArray: [1, 2, 3]
+    });
 });
 
+router.get('/test/:id', function (req, res, next) {
+    res.render('test', {
+        outputId: req.params.id
+    });
+});
+
+router.post('/test/submit', function (req, res, next) {
+    // in POST request all parameters concatenated
+    // and you will access to it by using .body
+    var id = req.body.id;
+    res.redirect('/test/' + id);
+});
 module.exports = router;
