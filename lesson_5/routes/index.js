d0c1b66c0b794ed95f99794c91123048e3471452
panda <SmelayaPanda@mail.ru>
2018-02-11 20:39:38 +0700

    lesson 5 - jade html processor

--- lesson_5/routes/index.js
+++ lesson_5/routes/index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index',
+        {
+            title: 'Express',
+            condition: true
+        }
+    );
 });
 
-router.get('/news/1', function(req, res, next) {
-  res.send('First news');
+
+router.get('/news/1', function (req, res, next) {
+    res.send('First news');
 });
 
+router.get('/test', function (req, res, next) {
+    res.render('test',
+        {
+            title: 'Panda, hello! =) ',
+            condition: true
+        }
+    );
+});
 module.exports = router;
