ca205e0f1996c8e0cab0f3d6306367e76df6990b
panda <SmelayaPanda@mail.ru>
2018-02-12 20:55:51 +0700

    lesson 9 mongo get and insert with routing

---
+++ lesson_10/routes/index.js
@@ -0,0 +1,64 @@
+let express = require('express');
+let router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+let assert = require('assert'); // build in package for check db connection is right and other comparisons check
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'test';
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+router.get('/get-data', function (req, res, next) {
+    let resultArray = [];
+    MongoClient.connect(url, function (err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+        let cursor = db.collection('user-data').find();
+        cursor.forEach(function (doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function (mongoError) { // another callback.
+            // If you type it below, code will be executed immediately
+            // not waiting retrieving data from database
+            client.close();
+            res.render('index', {items: resultArray});
+        })
+    });
+});
+
+router.post('/insert', function (req, res, next) {
+    console.log(req.body);
+    let item = {
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+    };
+
+    MongoClient.connect(url, function (err, client) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        db.collection('user-data').insertOne(item, function (err, result) {
+            assert.equal(null, err);
+            console.log('data will be inserted!');
+            client.close();
+        })
+    });
+
+    res.redirect('/');
+});
+
+router.post('/update', function (req, res, next) {
+});
+
+router.post('/delete', function (req, res, next) {
+});
+
+
+module.exports = router;
